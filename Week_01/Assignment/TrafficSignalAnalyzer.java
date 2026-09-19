c2df40551c66b65273cbb3798517b6488a97fa81
Azam-2006 <ma3968@srmist.edu.in>
2026-09-19 10:58:02 +0530

    Create TrafficSignalAnalyzer.java

---
+++ Week_01/Assignment/TrafficSignalAnalyzer.java
@@ -0,0 +1,41 @@
+public class TrafficSignalAnalyzer {
+
+    public static void findLongestStreak(String signalLog) {
+        if (signalLog == null || signalLog.isEmpty()) {
+            System.out.println("Log is empty.");
+            return;
+        }
+
+        char longestColor = signalLog.charAt(0);
+        int maxStreak = 1;
+
+        char currentColor = signalLog.charAt(0);
+        int currentStreak = 1;
+
+        for (int i = 1; i < signalLog.length(); i++) {
+            if (signalLog.charAt(i) == currentColor) {
+                currentStreak++;
+            } else {
+                if (currentStreak > maxStreak) {
+                    maxStreak = currentStreak;
+                    longestColor = currentColor;
+                }
+                currentColor = signalLog.charAt(i);
+                currentStreak = 1;
+            }
+        }
+
+        // Final check for trailing streak
+        if (currentStreak > maxStreak) {
+            maxStreak = currentStreak;
+            longestColor = currentColor;
+        }
+
+        System.out.println("Longest Streak: '" + longestColor + "' repeated " + maxStreak + " times");
+    }
+
+    public static void main(String[] args) {
+        findLongestStreak("RRGGGYRR");
+        findLongestStreak("RRRRYYGG");
+    }
+}
